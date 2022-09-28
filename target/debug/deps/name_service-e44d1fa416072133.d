9543b5162948ac5bb944cdf2ccbb6a605cd35946
“PTKhuong96” <phankhuong96@gmail.com>
2022-09-28 13:22:31 +0000

    Initial Commit

---
+++ target/debug/deps/name_service-e44d1fa416072133.d
@@ -0,0 +1,13 @@
+/home/ubuntu/ftrade_contracts/name-service/target/debug/deps/name_service-e44d1fa416072133.rmeta: src/lib.rs src/error.rs src/execute.rs src/msg.rs src/query.rs src/state.rs src/utils.rs
+
+/home/ubuntu/ftrade_contracts/name-service/target/debug/deps/name_service-e44d1fa416072133.d: src/lib.rs src/error.rs src/execute.rs src/msg.rs src/query.rs src/state.rs src/utils.rs
+
+src/lib.rs:
+src/error.rs:
+src/execute.rs:
+src/msg.rs:
+src/query.rs:
+src/state.rs:
+src/utils.rs:
+
+# env-dep:CARGO_PKG_VERSION=0.1.0
